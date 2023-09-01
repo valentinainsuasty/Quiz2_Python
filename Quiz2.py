36c0ebeee14b0ecdd12299aad9f317baefc10e9b
valentinainsuasty <valentina01010206@gmail.com>
2023-09-01 14:49:52 -0500

    Se realizó descifrado

---
+++ Quiz2.py
@@ -0,0 +1,23 @@
+print("Ingresa el mensaje")
+mensaje = input()
+print("Cuántas veces quieres mover la letra")
+veces = int(input())
+
+mensajeCifrado = ""
+
+for i in range(0, len(mensaje), 1):
+    letra = mensaje[i]
+    minuscula = (letra >= 'a' and letra <= 'z')
+    mayuscula = (letra >= 'A' and letra <= 'Z')
+    if not(minuscula or mayuscula):
+        mensajeCifrado += letra
+    else:
+        ascii = ord(letra)
+        baseAscii = ord('a')
+        if mayuscula:
+            baseAscii = ord('A')
+        nuevoAscii = (ascii - baseAscii - veces+26) % 26 + baseAscii
+        nuevaLetra = chr(nuevoAscii)
+        mensajeCifrado += nuevaLetra
+
+print(mensajeCifrado)
